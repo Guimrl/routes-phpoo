b4988e82cd8ea747944ef0d325811304cf36bc7e
Guilherme Amaral <88007295+Guimrl@users.noreply.github.com>
2022-12-21 23:52:08 -0300

    criando rotas simples

---
+++ app/core/Router.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace app\core;
+
+use app\core\RoutersFilter;
+
+class Router
+{
+    public static function run()
+    {
+        $routerRegistered = new RoutersFilter;
+        $router = $routerRegistered->get();
+
+        dd($router);
+    }
+}
