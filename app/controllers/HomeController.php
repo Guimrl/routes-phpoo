5a2a28a43d6076f575ad28fddfb7bd89a094d5d4
Guilherme Amaral <88007295+Guimrl@users.noreply.github.com>
2023-02-04 15:31:42 -0300

    criando conexão com o banco de dados

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -2,10 +2,16 @@
 
 namespace app\controllers;
 
+use app\database\models\User;
+
 class HomeController extends Controller
 {
     public function index()
     {
+
+        $user = new User;
+        $user->fetch();
+        
         $this->view('home', ['title' => 'Home']);
     }
 }
